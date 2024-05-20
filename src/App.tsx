09536dfc17d4b3e22f29fbfe89682809d3208f0b
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-20 20:49:54 +0100

    fixing some code commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { motion, useScroll, useSpring } from "framer-motion";
 const Navbar = lazy(() => import("./Components/Navbar/Navbar.tsx"));
 const Home = lazy(() => import("./Components/Home/Home.tsx"));
 const Services = lazy(() => import("./Components/Services/Services.tsx"));
-const NewWebSite = lazy(() => import("./Components/NewWebSite/NewWebSite.tsx"));
 const References = lazy(() => import("./Components/References/References.tsx"));
 const Blog = lazy(() => import("./Components/Blog/Blog.tsx"));
 const AboutUs = lazy(() => import("./Components/AboutUs/AboutUs.tsx"));
-const Loader = lazy(() => import("./Ui/Loader/Loader.tsx"));
 const Up = lazy(() => import("./Ui/Up/Up.tsx"));
 const ContactUs = lazy(() => import("./Components/ContactUs/ContactUs.tsx"));
 const Footer = lazy(() => import("./Ui/Footer/Footer.tsx"));
@@ -29,28 +27,19 @@ const App = () => {
         style={{ scaleX }}
       />
 
-      <Suspense
-        fallback={
-          <div className="mt-20 text-center">
-            <Loader />
-          </div>
-        }
-      >
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Accueil" element={<Home />} />
-          <Route path="/Serviclim Shop" element={<NewWebSite />} />
-          <Route path="/Nos Services" element={<Services />} />
-          <Route path="/Nos Références" element={<References />} />
-          <Route path="/Blog" element={<Blog />} />
-          <Route path="/À Propos de Nous" element={<AboutUs />} />
-          <Route path="/Contactez-Nous" element={<ContactUs />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Up />
-        <Footer />
-      </Suspense>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Accueil" element={<Home />} />
+        <Route path="/Nos Services" element={<Services />} />
+        <Route path="/Nos Références" element={<References />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/À Propos de Nous" element={<AboutUs />} />
+        <Route path="/Contactez-Nous" element={<ContactUs />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Up />
+      <Footer />
     </>
   );
 };
